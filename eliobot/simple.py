a083f0af66e33267f4ad1a9402fff58977afb7bf
EpiCanard <epicanard@gmail.com>
2026-05-29 21:10:04 +0200

    Add bluetooth server

--- eliobot/simple.py
+++ eliobot/simple.py
@@ -1,49 +1,61 @@
 import time
 import board
+import digitalio
+import neopixel
 
 from adafruit_ble import BLERadio
 from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
 from adafruit_ble.services.nordic import UARTService
 
 ble = BLERadio()
-ble.name = "Eliobot"
+BLE_NAME = "Eliobot"
+ble.name = BLE_NAME
 uart = UARTService()
 advertisement = ProvideServicesAdvertisement(uart)
+advertisement.complete_name = BLE_NAME
 
-led = getattr(board, "LED", None)
+buzzer_power = digitalio.DigitalInOut(board.IO17)
+buzzer_power.direction = digitalio.Direction.OUTPUT
+buzzer_power.value = False
 
-def set_led(state):
-    if led is None:
-        return
-    import digitalio
-    global led_io
-    try:
-        led_io
-    except NameError:
-        led_io = digitalio.DigitalInOut(led)
-        led_io.direction = digitalio.Direction.OUTPUT
-    led_io.value = state
+pixel = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.3)
+
+def blink(color, timer, repeat = 1):
+    for i in range (0, repeat):
+        pixel.fill(color)
+        pixel.show()
+        time.sleep(timer)
+
+        pixel.fill((0, 0, 0))
+        pixel.show()
+        time.sleep(timer)
 
 print("Demarrage BLE")
-ble.start_advertising(advertisement)
 
 while True:
-    while not ble.connected:
-        set_led(False)
-        time.sleep(0.1)
+    if ble.advertising:
+        ble.stop_advertising()
 
+    ble.start_advertising(advertisement)
+    print("En attente de connexion BLE")
+
+    while not ble.connected:
+        blink((0, 0, 255), 0.1)
+
+    ble.stop_advertising()
     print("Client connecte")
-    set_led(True)
-    uart.write("Bonjour depuis Eliobot BLE!\r\n")
+    blink((0, 255, 0), 0.1, 2)
 
     while ble.connected:
         if uart.in_waiting:
             data = uart.read(uart.in_waiting)
             if data:
                 print("Recu:", data)
-                uart.write(b"Echo: " + data)
+                try:
+                    uart.write(b"Echo: " + data)
+                except OSError as exc:
+                    print("Envoi BLE impossible:", exc)
         time.sleep(0.05)
 
     print("Client deconnecte")
-    set_led(False)
-    ble.start_advertising(advertisement)
+    time.sleep(0.5)
